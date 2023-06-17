592057baae1770a774e5e4d4c0ed425dc7c13eee
Ruslan Kornilov <kornilov_ruslan51@mail.ru>
2023-06-17 16:43:58 +0300

    first commit

---
+++ src/Worker.java
@@ -0,0 +1,35 @@
+@FunctionalInterface
+interface OnTaskDoneListener {
+    void onDone(String result);
+}
+
+@FunctionalInterface
+interface OnTaskErrorListener {
+    void onError(String result);
+}
+
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            try {
+                if (i == 33) {
+                    throw new RuntimeException("Task " + i + " failed");
+                } else {
+                    callback.onDone("Task " + i + " is done");
+                }
+            } catch (Exception e) {
+                errorCallback.onError(e.getMessage());
+            }
+        }
+    }
+
+}
+
